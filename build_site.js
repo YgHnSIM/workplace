b4eadce65ee4e7fff531fdd03b32b9f13e383923
sim <globeboxai@gmail.com>
2026-07-07 08:20:07 +0900

    Stabilize archive metadata layout

--- build_site.js
+++ build_site.js
@@ -14,7 +14,7 @@ const rootDir = __dirname;
 const catalogPath = path.join(rootDir, '_source', 'catalog.json');
 const momManifestPath = path.join(rootDir, '_source', 'generated', 'mom.json');
 const homeFilePath = path.join(rootDir, 'index.html');
-const assetVersion = '20260707-2';
+const assetVersion = '20260707-3';
 
 const categoryLabels = {
   all: '전체',
